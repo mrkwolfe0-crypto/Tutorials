4f90f546096c9e4d24be905df204dff4745479de
mrkwolfe0-crypto <mrkwolfe0@gmail.com>
2026-07-21 20:27:42 -0400

    update

---
+++ Calculator/Java/App.java
@@ -0,0 +1,6 @@
+//This is the entry point file that initializes and starts the calculator application.
+public class App {
+    public static void main(String[] args) throws Exception {
+        Calculator calculator = new Calculator();
+    }
+}
