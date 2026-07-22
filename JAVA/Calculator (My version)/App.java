bc09d00291a0206909e6e9458e1929f6e2511baa
mrkwolfe0-crypto <mrkwolfe0@gmail.com>
2026-07-22 05:15:01 -0400

    Update App.java

--- JAVA/Calculator (My version)/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        new Calculator() ;
-    }
-}
-//This is the entry point file that initializes and starts the calculator application.
