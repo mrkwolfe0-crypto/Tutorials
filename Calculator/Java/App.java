f0f1c8c11707725c0994d76b3c36d59b64f13a04
mrkwolfe0-crypto <mrkwolfe0@gmail.com>
2026-07-21 20:12:03 -0400

    update

--- Calculator/Java/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[]args) throws Exception {
-        Calculator calculator = new Calculator();
-    }
-}
-//Run this to run the app not the calculator.java file
