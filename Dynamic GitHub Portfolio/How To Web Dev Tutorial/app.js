50511e7ea80c3e517b3cb9884476250c04946785
Mark Wolfe <270744027+mrkwolfe0-crypto@users.noreply.github.com>
2026-07-27 06:15:44 -0400

    j

--- Dynamic GitHub Portfolio/How To Web Dev Tutorial/app.js
+++ Dynamic GitHub Portfolio/How To Web Dev Tutorial/app.js
@@ -17,3 +17,11 @@ Personal Modifications:
 - Documented DOM interaction.
 -----------------------------------------------------
 */
+
+let menu = document.querySelector('#menu');
+let links = document.querySelector('.links');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  links.classList.toggle('active');
+};
