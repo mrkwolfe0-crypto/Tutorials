62bfbc677d578ebe5ec25607761901a780d8bb45
mrkwolfe0-crypto <mrkwolfe0@gmail.com>
2026-07-27 20:10:17 -0400

    Deleting source files
    Co-pilot keeps telling me to use <li><a href=...
    When in the tutorial he uses only <a> so to keep a base line I will keep it to the tutorial. I also will be seeing where I can add my own parts I want like the GitHub repository live feed, and change some of the things he places in the tutorial from hire me to resume, and follow the embedding tutorial for my dedicated embeeded resume, and the about me is staying. Then borrow the state manager not the aeroglass as originally planned. Having the methodology that allowed login and registration to switch as shown in Login and Registration page tutorial swap out my resume dynamic live feed and the about me. And now I will combine my skills into the about me segment.

--- Dynamic GitHub Portfolio/How To Web Dev Tutorial/Starter Files/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu');
-let links = document.querySelector('.links');
-
-menu.onclick = () => {
-  menu.classList.toggle('bx-x');
-  links.classList.toggle('active');
-};
